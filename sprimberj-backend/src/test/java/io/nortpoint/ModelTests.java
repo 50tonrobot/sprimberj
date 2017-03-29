3a1398b527250a8b48a764db3cbcc1eaf865dd8b
50tonrobot <50tonrobot@gmail.com>
2017-03-29 00:10:29 -0400

    temporarily fixes context config until I have real tests

--- sprimberj-backend/src/test/java/io/nortpoint/ModelTests.java
+++ sprimberj-backend/src/test/java/io/nortpoint/ModelTests.java
@@ -6,9 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = StartApplication.class)
+@ContextConfiguration(loader=AnnotationConfigContextLoader.class)
 public class ModelTests {
 	
 	@Test
